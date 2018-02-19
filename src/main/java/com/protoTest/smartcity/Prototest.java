99c40411b107b34b9273b872889c78eba2ab61b7
Sahil Tyagi <sahiltyagi@Sahils-MacBook.local>
2018-02-19 14:21:20 +0530

    pushing proto generated code repo to github

---
+++ src/main/java/com/protoTest/smartcity/Prototest.java
@@ -0,0 +1,11 @@
+package com.protoTest.smartcity;
+
+public class Prototest {
+	public static void start() {
+		System.out.println("inside this prototest method");
+	}
+	
+	public static void main(String[] args) {
+		start();
+	}
+}
---
+++ src/main/java/com/protoTest/smartcity/camelcasetest.java
@@ -0,0 +1,31 @@
+package com.protoTest.smartcity;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import org.metawidget.util.simple.StringUtils;
+
+public class camelcasetest {
+
+	public static void main(String[] args) {
+//		String s1= StringUtils.camelCase("actuated_qwe-1236", '_');
+//		s1 = StringUtils.camelCase(s1, '-');
+//		System.out.println(s1);
+		
+//		ConcurrentLinkedQueue<ConcurrentLinkedQueue<byte[]>> qw = new ConcurrentLinkedQueue<ConcurrentLinkedQueue<byte[]>>();
+//		ConcurrentLinkedQueue<byte[]> q = new ConcurrentLinkedQueue<byte[]>();
+//		String devid = "RBCCPS__001";
+//		String data = "{\"autoluxparam\":1000}";
+//		
+//		//q.add(devid.getBytes());
+//		q.add(data.getBytes());
+//		q.add(devid.getBytes());
+//		qw.add(q);
+//		
+//		ConcurrentLinkedQueue<byte[]> as = qw.poll();
+//		System.out.println(new String(as.poll()));
+//		System.out.println(new String(as.poll()));
+		
+	}
+}
